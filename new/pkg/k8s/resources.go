1e2edf072a3377f3bcd8d356a9ea7ab057e49803
Daniel Booth <user@danielbooth.cloud>
2024-12-21 14:33:43 +0000

    feat: improved functionality

--- new/pkg/k8s/resources.go
+++ new/pkg/k8s/resources.go
@@ -1,7 +1,7 @@
 package k8s
 
 import (
-	"strings"
+	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
@@ -62,71 +62,124 @@ func (r *Resources) FindRelatedResources(workload metav1.Object, podSpec *corev1
 
 	workloadLabels := workload.GetLabels()
 	workloadName := workload.GetName()
+	
+	// Get workload kind
+	var workloadKind string
+	switch workload.(type) {
+	case *appsv1.StatefulSet:
+		workloadKind = "StatefulSet"
+	case *appsv1.Deployment:
+		workloadKind = "Deployment"
+	case *appsv1.DaemonSet:
+		workloadKind = "DaemonSet"
+	case *batchv1.Job:
+		workloadKind = "Job"
+	case *batchv1.CronJob:
+		workloadKind = "CronJob"
+	default:
+		workloadKind = "Unknown"
+	}
 
-	// Find related Services
+	// Find related Services - don't use found map for services since they can be shared
 	for i, svc := range r.Services.Items {
 		if svc.Spec.Selector == nil {
 			continue
 		}
 
-		matches := true
-		for key, value := range svc.Spec.Selector {
-			if workloadLabels[key] != value {
-				matches = false
-				break
+		matches := false
+		// Check if service selector matches workload labels
+		if len(svc.Spec.Selector) > 0 {
+			matches = true
+			for key, value := range svc.Spec.Selector {
+				if workloadLabels[key] != value {
+					matches = false
+					break
+				}
 			}
 		}
-		
-		if !matches && (strings.HasPrefix(svc.Name, workloadName) || 
-					  strings.HasSuffix(svc.Name, workloadName)) {
-			matches = true
+
+		// For StatefulSets, also check if service name matches workload name
+		if !matches && workloadKind == "StatefulSet" {
+			if svc.Name == workloadName || 
+			   svc.Name == workloadName+"-headless" {
+				matches = true
+			}
 		}
 
 		if matches {
-			key := "Service/" + svc.Name
-			if !found[key] {
-				services = append(services, &r.Services.Items[i])
-				found[key] = true
-			}
+			services = append(services, &r.Services.Items[i])
 		}
 	}
 
-	// Find related resources from volumes
-	for _, vol := range podSpec.Volumes {
-		if vol.ConfigMap != nil {
-			key := "ConfigMap/" + vol.ConfigMap.Name
-			if !found[key] {
-				for i, cm := range r.ConfigMaps.Items {
-					if cm.Name == vol.ConfigMap.Name {
-						configMaps = append(configMaps, &r.ConfigMaps.Items[i])
-						found[key] = true
-						break
+	// Find related resources from volumes and environment
+	if podSpec != nil {
+		// Check volumes
+		for _, vol := range podSpec.Volumes {
+			if vol.ConfigMap != nil {
+				key := fmt.Sprintf("%s/ConfigMap/%s", workloadKind, vol.ConfigMap.Name)
+				if !found[key] {
+					for i, cm := range r.ConfigMaps.Items {
+						if cm.Name == vol.ConfigMap.Name {
+							configMaps = append(configMaps, &r.ConfigMaps.Items[i])
+							found[key] = true
+							break
+						}
+					}
+				}
+			}
+
+			if vol.Secret != nil {
+				key := fmt.Sprintf("%s/Secret/%s", workloadKind, vol.Secret.SecretName)
+				if !found[key] {
+					for i, secret := range r.Secrets.Items {
+						if secret.Name == vol.Secret.SecretName {
+							secrets = append(secrets, &r.Secrets.Items[i])
+							found[key] = true
+							break
+						}
+					}
+				}
+			}
+
+			if vol.PersistentVolumeClaim != nil {
+				key := fmt.Sprintf("%s/PVC/%s", workloadKind, vol.PersistentVolumeClaim.ClaimName)
+				if !found[key] {
+					for i, pvc := range r.PVCs.Items {
+						if pvc.Name == vol.PersistentVolumeClaim.ClaimName {
+							pvcs = append(pvcs, &r.PVCs.Items[i])
+							found[key] = true
+							break
+						}
 					}
 				}
 			}
 		}
 
-		if vol.Secret != nil {
-			key := "Secret/" + vol.Secret.SecretName
-			if !found[key] {
-				for i, secret := range r.Secrets.Items {
-					if secret.Name == vol.Secret.SecretName {
-						secrets = append(secrets, &r.Secrets.Items[i])
-						found[key] = true
-						break
+		// Check environment variables
+		for _, container := range podSpec.Containers {
+			for _, env := range container.EnvFrom {
+				if env.ConfigMapRef != nil {
+					key := fmt.Sprintf("%s/ConfigMap/%s", workloadKind, env.ConfigMapRef.Name)
+					if !found[key] {
+						for i, cm := range r.ConfigMaps.Items {
+							if cm.Name == env.ConfigMapRef.Name {
+								configMaps = append(configMaps, &r.ConfigMaps.Items[i])
+								found[key] = true
+								break
+							}
+						}
 					}
 				}
-			}
-		}
-
-		if vol.PersistentVolumeClaim != nil {
-			key := "PVC/" + vol.PersistentVolumeClaim.ClaimName
-			if !found[key] {
-				for i, pvc := range r.PVCs.Items {
-					if pvc.Name == vol.PersistentVolumeClaim.ClaimName {
-						pvcs = append(pvcs, &r.PVCs.Items[i])
-						found[key] = true
-						break
+				if env.SecretRef != nil {
+					key := fmt.Sprintf("%s/Secret/%s", workloadKind, env.SecretRef.Name)
+					if !found[key] {
+						for i, secret := range r.Secrets.Items {
+							if secret.Name == env.SecretRef.Name {
+								secrets = append(secrets, &r.Secrets.Items[i])
+								found[key] = true
+								break
+							}
+						}
 					}
 				}
 			}
